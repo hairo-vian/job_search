76c727c64bac6b3b6d91a91c5d2aede827870322
Asvian S <a.sulaeman@teravintech.com>
2022-09-14 16:31:01 +0700

    Initial commit

---
+++ lib/ui/sample/sample.dart
@@ -0,0 +1,17 @@
+import 'dart:math';
+
+class Sample {
+  int r = Random().nextInt(10);
+  int a = 0;
+  String abc = "cba";
+  var aofnoaenfoaenfoaeinf = "afolenoiefanopiaefn";
+}
+
+class B {
+  void func(){
+    var s = Sample();
+    s.a = 1;
+
+    var s2 = Sample();
+  }
+}
